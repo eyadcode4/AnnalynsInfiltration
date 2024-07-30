830cef4d35092892d0132d7c72c5166a02732fc0
eyadcode24 <eyadcode24@gmail.com>
2024-07-30 09:11:06 +0200

    final

---
+++ src/main/java/eyad/dev/AnnalynsInfiltration.java
@@ -0,0 +1,24 @@
+package eyad.dev;
+
+public class AnnalynsInfiltration {
+    public static boolean canFastAttack(boolean knightIsAwake) {
+        // Implement method
+        return !knightIsAwake;
+    }
+
+    public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
+        // Implement method
+        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+    }
+
+    public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
+        // Implement method
+        return !archerIsAwake && prisonerIsAwake;
+    }
+
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
+            boolean petDogIsPresent) {
+        // Implement method
+        return (!knightIsAwake && ! archerIsAwake && petDogIsPresent) || (!archerIsAwake && petDogIsPresent);
+    }
+}
